a3e59571f098a9da949fed0ceefab9f13c1f68f3
Dnyandaa18 <dnyandataur007@gmail.com>
2026-01-20 18:23:43 +0530

    Adding math operations

---
+++ math_functions.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    //     int x = 9;
+    //     x = sqrt(x);
+    //     printf("The Square of x is: %d\n", x);
+
+    //     int y = 2;
+    //     y = pow(y, 3);
+    //     printf("The power of y is: %d\n", y);
+
+    float x = 45;
+
+    // x = round(x);
+    //  x = ceil(x);
+    // x = floor(x);
+    // x = abs(x);
+    //  x = log(x);
+    x = sin(x);
+    printf("%f\n", x);
+    return 0;
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,5 @@
+
+{
+    int x = 9;
+    x = sqrt(x);
+    printf("The Square of x is: %d\n", x);
